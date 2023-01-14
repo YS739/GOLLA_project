747e172ff054bc7b3f5d72b5258c316577bc17e9
YS739 <suk921@gmail.com>
2023-01-14 16:25:15 +0900

    Edit tanstack to react query:

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import store from "./redux/config/configStore";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import store from './redux/config/configStore';
+import { Provider } from 'react-redux';
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
     </Provider>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
