4d5a411681d3a901a8abe26b6b882162765cac61
YS739 <suk921@gmail.com>
2023-01-14 16:28:38 +0900

    Remove store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import store from './redux/config/configStore';
 
 const queryClient = new QueryClient();
 
