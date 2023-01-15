ca3369464d0ef64ad40638af067cd2e9918af507
heeryoung <winterbooks@naver.com>
2023-01-15 13:19:41 +0900

    feat: Header UI 완성 로고, POST navigate 연결

--- src/shared/Layout/Header/Header.jsx
+++ src/shared/Layout/Header/Header.jsx
@@ -1,4 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { StyledHeader, HeaderContainer, Logo, HeaderBtnBox, HeaderBtn } from "./style";
+
 const Header = () => {
-  return;
+
+  const navigate = useNavigate();
+  
+  return (
+    <StyledHeader>
+      <HeaderContainer>
+        {/* <Logo src="../../../assets/Logo.png" /> */}
+        <Logo 
+        onClick={() => navigate("/")}
+        src="https://user-images.githubusercontent.com/117061017/212521539-db30b6d5-730a-4941-83a0-805f97cb157f.png" 
+        />
+        <HeaderBtnBox>
+          <HeaderBtn onClick={() => navigate("/upload")}>POST</HeaderBtn>
+          <HeaderBtn>LOGIN</HeaderBtn>
+        </HeaderBtnBox>
+      </HeaderContainer>
+    </StyledHeader>
+  )
 };
+
 export default Header;
