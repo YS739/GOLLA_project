5fdab0588ed30e379a3adb270b02007514bb9b19
hwan910 <psh5575@gmail.com>
2023-01-26 11:17:35 +0900

    feat 구글 깃헙 간편로그인 구현

--- src/shared/Layout/LoginModal/LoginModal.tsx
+++ src/shared/Layout/LoginModal/LoginModal.tsx
@@ -14,25 +14,72 @@ import {
   InputText,
   CompleteBtn,
 } from './style';
-
 import { IoCloseSharp } from 'react-icons/io5';
 import { IoIosArrowBack } from 'react-icons/io';
 import { colors } from '../../../common/color';
+import { isOpenPropsP } from '../../../common/interfaces';
 
-const LoginModal = ({ isOpen, setIsOpen }: any) => {
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
+import { authService } from '../../../common/firebase';
+
+const LoginModal: React.FunctionComponent<isOpenPropsP> = ({
+  isOpen,
+  setIsOpen,
+}) => {
   const [dp, setDp] = useState(0);
   const closeModal = () => {
     setIsOpen(false);
     setDp(0);
   };
+
+  const GoogleProvider = new GoogleAuthProvider();
+  const GitHubProvider = new GithubAuthProvider();
+  authService.languageCode = 'ko';
+
+  const GoogleJoinHandler = () => {
+    signInWithPopup(authService, GoogleProvider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        setDp(1);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const crediential = GoogleAuthProvider.credentialFromError(error);
+      });
+  };
+
+  const GitHubJoinHandler = () => {
+    signInWithPopup(authService, GitHubProvider)
+      .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        setDp(1);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const crediential = GithubAuthProvider.credentialFromError(error);
+      });
+  };
+
   return (
     <>
       <ReactModal isOpen={isOpen} style={customStyle}>
         {dp === 0 && (
           <Modal>
-            {/* <Btn setIsOpen={setIsOpen} onClick={closeModal}>
+            <Btn onClick={closeModal}>
               <IoCloseSharp />
-            </Btn> */}
+            </Btn>
             <GuideBox>
               <img
                 src={require('../../../assets/Logo.png')}
@@ -47,17 +94,27 @@ const LoginModal = ({ isOpen, setIsOpen }: any) => {
                   style={{ padding: 10, width: 80, height: 80 }}
                   src="https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png"
                   onClick={() => {
-                    setDp(1);
+                    GoogleJoinHandler();
                   }}
                 />
                 <IconText>Google 로그인</IconText>
               </IconDiv>
               <IconDiv>
-                <Icon src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" />
+                <Icon
+                  src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU"
+                  onClick={() => {
+                    GitHubJoinHandler();
+                  }}
+                />
                 <IconText>Github 로그인</IconText>
               </IconDiv>
               <IconDiv>
-                <Icon src="https://t1.daumcdn.net/crms/symbol_img/symbol_%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%86%A1.png" />
+                <Icon
+                  src="https://t1.daumcdn.net/crms/symbol_img/symbol_%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%86%A1.png"
+                  onClick={() => {
+                    alert('준비중입니다. 다른 방법을 이용해주세요.');
+                  }}
+                />
                 <IconText>Kakao 로그인</IconText>
               </IconDiv>
             </IconBox>
@@ -74,9 +131,9 @@ const LoginModal = ({ isOpen, setIsOpen }: any) => {
               >
                 <IoIosArrowBack />
               </Btn>
-              {/* <Btn setIsOpen={setIsOpen} onClick={closeModal}>
+              <Btn onClick={closeModal}>
                 <IoCloseSharp />
-              </Btn> */}
+              </Btn>
             </ModalDiv>
             <GuideBox>
               <img
@@ -114,9 +171,9 @@ const LoginModal = ({ isOpen, setIsOpen }: any) => {
               >
                 <IoIosArrowBack />
               </Btn>
-              {/* <Btn setIsOpen={setIsOpen} onClick={closeModal}>
+              <Btn onClick={closeModal}>
                 <IoCloseSharp />
-              </Btn> */}
+              </Btn>
             </ModalDiv>
 
             <GuideBox>가입이 완료 되었습니다.</GuideBox>
