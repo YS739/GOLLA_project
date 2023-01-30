00dd7fb8b9e8c488e6e7ffb36157d41494a822b1
YS739 <suk921@gmail.com>
2023-01-30 20:52:51 +0900

    Edit 중복 코드 피하기 및 주석 삭제

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,1 +1,11 @@
-declare interface Something {}
+// Post type
+declare interface postJ {
+  title: string | number;
+  content: string | number;
+  createdAt: Date;
+  categoryA: string | number;
+  categoryB: string | number;
+  likes?: [];
+  // userId: string;
+  // nickName: string;
+}
--- src/pages/UploadPage/UploadPage.tsx
+++ src/pages/UploadPage/UploadPage.tsx
@@ -1,44 +1,10 @@
 import { useRef, useState, FC, ChangeEvent, MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { colors } from '../../common/color';
-import {
-  Section,
-  Article,
-  PostForm,
-  WritingBox,
-  PostTitleBox,
-  PostTitle,
-  TitleInput,
-  PostContentBox,
-  PostContent,
-  ContentTextarea,
-  CategoryBox,
-  ABox,
-  CategoryA,
-  BBox,
-  CategoryB,
-  AddBtn,
-} from './style';
+import * as S from './style';
 import { collection } from 'firebase/firestore';
 import { db } from '../../common/firebase';
 import { useFirestoreCollectionMutation } from '@react-query-firebase/firestore';
-
-// 1. type 정의 - string | number??
-// 2. input 연결
-// 3. 등록 버튼 함수 만들기
-// 3. firebase addDoc
-// 4. useMutation 쓰기
-
-interface postJ {
-  title: string | undefined;
-  content: string | undefined;
-  createdAt: Date;
-  categoryA: string | undefined;
-  categoryB: string | undefined;
-  likes?: [];
-  // userId: string;
-  // nickName: string;
-}
 
 type UploadPageJ = () => any;
 
@@ -47,64 +13,44 @@ const UploadPage: UploadPageJ = () => {
 
   const ref = collection(db, 'posts');
   const mutation = useFirestoreCollectionMutation(ref);
-  // const [title, setTitle] = useState<string>('');
-  // const [content, setContent] = useState<string>('');
-  // const [categoryA, setCategoryA] = useState<string>('');
-  // const [categoryB, setCategoryB] = useState<string>('');
 
   const title_input = useRef<HTMLInputElement>(null);
   const content_textarea = useRef<HTMLTextAreaElement>(null);
   const categoryA_input = useRef<HTMLInputElement>(null);
   const categoryB_input = useRef<HTMLInputElement>(null);
 
-  //  input에 입력된 값 가져오기
-  // const InputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault();
-  //   if (e.target.name === 'title') {
-  //     setTitle(e.target.value);
-  //   }
-  //   if (e.target.name === 'cA') {
-  //     setCategoryA(e.target.value);
-  //   }
-  //   if (e.target.name === 'cB') {
-  //     setCategoryB(e.target.value);
-  //   }
-  // };
-  // // content = textarea에 입력된 값 가져오기
-  // const TextareaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-  //   e.preventDefault();
-
-  //   setContent(e.target.value);
-  // };
+  const title = title_input.current?.value;
+  const content = content_textarea.current?.value;
+  const categoryA = categoryA_input.current?.value;
+  const categoryB = categoryB_input.current?.value;
 
   // FIXME: form에 연결하면 type 에러
   const PostBtnHandler = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const newPost: postJ = {
-      title: title_input.current?.value,
-      content: content_textarea.current?.value,
-      createdAt: new Date(),
-      // TODO: new Date().toLocaleString("ko-KR") 로 변경?
-      categoryA: categoryA_input.current?.value,
-      categoryB: categoryB_input.current?.value,
-      // likes: [],
-      // userId: authService.currentUser.uid,
-      // nickName: authService.currentUser.displayname
-    };
-    //
-    if (title_input.current?.value.trim().length === 0) {
+    if (title?.trim().length === 0) {
       alert('제목을 입력해주세요.');
       title_input.current?.focus();
-    } else if (content_textarea.current?.value.trim().length === 0) {
+    } else if (content?.trim().length === 0) {
       alert('내용을 입력해주세요.');
       content_textarea.current?.focus();
-    } else if (categoryA_input.current?.value.trim().length === 0) {
-      alert('A카테고리 내용을 입력해주세요.');
-      categoryA_input.current.focus();
-    } else if (categoryB_input.current?.value.trim().length === 0) {
-      alert('B카테고리 내용을 입력해주세요.');
-      categoryB_input.current.focus();
-    } else {
+    } else if (categoryA?.trim().length === 0) {
+      alert('A 내용을 입력해주세요.');
+      categoryA_input.current?.focus();
+    } else if (categoryB?.trim().length === 0) {
+      alert('B 내용을 입력해주세요.');
+      categoryB_input.current?.focus();
+    } else if (title && content && categoryA && categoryB) {
+      const newPost: postJ = {
+        title: title,
+        content: content,
+        createdAt: new Date(),
+        // TODO: new Date().toLocaleString("ko-KR") 로 변경?
+        categoryA: categoryA,
+        categoryB: categoryB,
+        // likes: [],
+        // userId: authService.currentUser.uid,
+        // nickName: authService.currentUser.displayname
+      };
       mutation.mutate(newPost);
 
       // TODO: ${post.id}로 바꾸기
@@ -114,55 +60,35 @@ const UploadPage: UploadPageJ = () => {
   };
 
   return (
-    <Section>
-      <Article>
-        <PostForm>
-          <WritingBox>
-            <PostTitleBox>
-              <PostTitle>제목</PostTitle>
-              <TitleInput
-                ref={title_input}
-                name="title"
-                // value={title}
-                // onChange={InputHandler}
-              />
-            </PostTitleBox>
-            <PostContentBox>
-              <PostContent>내용</PostContent>
-              <ContentTextarea
-                ref={content_textarea}
-                name="content"
-                // value={content}
-                // onChange={TextareaHandler}
-              />
-            </PostContentBox>
-          </WritingBox>
-          <CategoryBox>
-            <ABox>
-              <CategoryA color={colors.RED}>A</CategoryA>
-              <input
-                ref={categoryA_input}
-                name="cA"
-                // value={categoryA}
-                // onChange={InputHandler}
-              />
-            </ABox>
-            <BBox>
-              <CategoryB color={colors.BLUE}>B</CategoryB>
-              <input
-                ref={categoryB_input}
-                name="cB"
-                // value={categoryB}
-                // onChange={InputHandler}
-              />
-            </BBox>
-          </CategoryBox>
+    <S.Section>
+      <S.Article>
+        <S.PostForm>
+          <S.WritingBox>
+            <S.PostTitleBox>
+              <S.PostTitle>제목</S.PostTitle>
+              <S.TitleInput ref={title_input} />
+            </S.PostTitleBox>
+            <S.PostContentBox>
+              <S.PostContent>내용</S.PostContent>
+              <S.ContentTextarea ref={content_textarea} />
+            </S.PostContentBox>
+          </S.WritingBox>
+          <S.CategoryBox>
+            <S.ABox>
+              <S.CategoryA color={colors.RED}>A</S.CategoryA>
+              <input ref={categoryA_input} />
+            </S.ABox>
+            <S.BBox>
+              <S.CategoryB color={colors.BLUE}>B</S.CategoryB>
+              <input ref={categoryB_input} />
+            </S.BBox>
+          </S.CategoryBox>
           <div style={{ width: '100%' }}>
-            <AddBtn onClick={PostBtnHandler}>등록</AddBtn>
+            <S.AddBtn onClick={PostBtnHandler}>등록</S.AddBtn>
           </div>
-        </PostForm>
-      </Article>
-    </Section>
+        </S.PostForm>
+      </S.Article>
+    </S.Section>
   );
 };
 
